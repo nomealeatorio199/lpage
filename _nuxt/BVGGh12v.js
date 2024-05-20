940ee8d06dd8940411f2045e3f2de8a6638ba30b
autoblocs <felipe.santos@autoblocs.com>
2024-05-20 00:17:28 -0300

    DELETE lpage

--- _nuxt/BVGGh12v.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./C_mt-4V3.js";import{_ as c,c as s,a as e,b as a,w as n,o as _}from"./BV96B_by.js";const i={},r={class:"w-full"},l={id:"hero",class:"w-full pb-24"},d=e("h1",null,"Política de Privacidade",-1);function f(p,u){const o=t;return _(),s("div",r,[e("section",l,[a(o,null,{default:n(()=>[d]),_:1})])])}const x=c(i,[["render",f]]);export{x as default};
--- _nuxt/CrMW9IML.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./DW_pA_kT.js";import{_ as s,c,a as e,b as n,w as a,o as _}from"./BloSLaj-.js";const r={},i={class:"w-full"},l={id:"hero",class:"w-full pb-24"},d=e("h1",null,"Política de Privacidade",-1);function f(m,u){const o=t;return _(),c("div",i,[e("section",l,[n(o,null,{default:a(()=>[d]),_:1})])])}const x=s(r,[["render",f]]);export{x as default};
